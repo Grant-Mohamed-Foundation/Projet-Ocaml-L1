959038c8a9f8465b338e310943ca76f73de4d97a
hugo <hugo@localhost.localdomain>
2017-04-11 15:53:45 +0200

    ceci est un test, challah ca fais pas nimporte quoi

--- partie1/list_sup.ml
+++ partie1/list_sup.ml
@@ -23,16 +23,15 @@ let rec selectionne inf l =
 				 else selectionne inf (y::r);;
 
 
+(********************** supprime **********************)
+
+
+
+
 (********************** tri_selection_min **********************)
 
 let rec tri_selection_min inf liste =
 	match liste with
-	[] -> []
-	|[x] -> [x]
-	|x::y::[] -> if inf x y
-				 then x::y::[]
-				 else y::x::[]
-				 
-	|x::y::r -> if inf x y
-				then x::selectionne inf (y::r) :: tri_selection_min inf r
-				else y::selectionne inf (x::r) :: tri_selection_min inf r;;
+	[] -> liste
+	|x::r -> selectionne inf liste :: tri_selection_min inf (supprime(selectionne inf l))
+	;;
