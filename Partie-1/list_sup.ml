3e75f567fe0a868f5af7a0e5ed56210df067e394
HellToShell <thomas.grant42@hotmail.fr>
2017-04-12 10:27:11 +0200

    Update de list_sup.ml

--- Partie-1/list_sup.ml
+++ Partie-1/list_sup.ml
@@ -1,52 +1,55 @@
 (* list_sup.ml *)
 
-      (***********************************)
-      (*                                 *)
-      (* Implantation du module list_sup *)
-      (*                                 *)
-      (***********************************)
+        (***********************************)
+        (*                                 *)
+        (* Implantation du module list_sup *)
+        (*                                 *)
+        (***********************************)
+        
+        
 
-
-
-(********************** selectionne **********************)
+(************* Tri par selection du minimum ******************)
+(**************************  1  ******************************)
 
 let rec selectionne inf l = 
-	match l with
-	[] -> failwith "Erreur liste vide"
-	|[x] -> x
-	|x::y::[] -> if inf x y
-				 then x
-				 else y
-				 
-	| x::y::r -> if inf x y
-				 then selectionne inf (x::r)
-				 else selectionne inf (y::r);;
-
-
-
-(************************  supprime  ******************************)
+    match l with
+    [] -> failwith "La liste est vide"
+    | x::[] -> x
+    | x::y::[] -> if inf x y
+                  then x
+                  else y
+    | x::(y::r) -> if inf x y
+                   then selectionne inf (x::r)
+                   else selectionne inf (y::r) ;;
+    
+(**************************  2  ******************************)
 
 let rec supprime x l =
     match l with
-    [] -> []
+    [] -> l
     |a::[] -> if a == x
               then []
               else [a]
-    |a::y::[] -> if a == x
-                 then [y]
-                 else supprime x [y]
-    |a::(y::r) -> if a == x
-                  then y::r
-                  else if y == x
-                       then a::r
-                       else a::(y::(supprime x r)) ;;
-
-
-
-(********************** tri_selection_min **********************)
+    |a::r -> if a == x
+             then r
+             else a::(supprime x r);;
+             
+(**************************  3  ******************************)
 
 let rec tri_selection_min inf liste =
-	match liste with
-	[] -> liste
-	|x::r -> selectionne inf liste :: tri_selection_min inf (supprime(selectionne inf l))
-	;;
+    match liste with
+    [] -> []
+    |[x] -> [x]
+    |x::(y::[]) -> if inf x y
+                 then x::y::[]
+                 else y::x::[]
+    |x::r -> (selectionne inf liste)::(tri_selection_min inf (supprime (selectionne inf liste) liste));;
+    
+    
+(**************************  4  ******************************)    
+
+
+
+
+
+
