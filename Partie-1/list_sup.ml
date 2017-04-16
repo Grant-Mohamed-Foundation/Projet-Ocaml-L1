72971734925f4ace46afb7331bba247a0301f867
ihfisk <ihfisk42@gmail.com>
2017-04-16 12:10:16 +0200

    ca avance laus ca ne marche pas

--- Partie-1/list_sup.ml
+++ Partie-1/list_sup.ml
@@ -7,9 +7,7 @@
         (***********************************)
         
         
-
-(************* Tri par selection du minimum ******************)
-
+        
 
 (**************************  selectionne  ******************************)
 
@@ -50,21 +48,27 @@ let rec tri_selection_min inf liste =
 
 (********************** partitionne **********************)
 
-let rec partitionne_bis liste listePaire listeImpaire =
+let rec partitionne_bis liste liste1 liste2 =
     match liste with
-    [] -> listePaire , listeImpaire
-    |[x] -> (x::listePaire) , listeImpaire
-    |x::y::r -> partitionne_bis r (x::listePaire) (y::listeImpaire);;
+    [] -> liste1 , liste2
+    |[x] -> (x::liste1) , liste2
+    |x::y::r -> partitionne_bis r (x::liste1) (y::liste2);; (* ici probleme car les listes sont inverser, donc [1;2;3;4;5;6] donne [5;3;1],[6;4;2] *)
 
 let partitionne liste = partitionne_bis liste [] [] ;;
 
 
 (********************** fusionne ***********************)
 
-let fusionne inf liste1 liste2 = if inf liste1 liste2
-                                 then liste1 @ liste2
-                                 else liste2 @ liste1;;
+(* elle doit prendre 2 listes et les fusionner de type [1;3;5] et [2;4;6] ca donne [1;2;3;4;5;6] *)
 
+let rec fusionne inf liste1 liste2 =
+    match (liste1,liste2) with
+    ([],[]) -> []
+    |([],_) -> liste2
+    |(_,[]) -> liste1
+    |(x::r1,y::r2) -> if inf x y
+                        then x::y::(fusionne inf r1 r2)
+                        else y::x::(fusionne inf r1 r2);;
 
 (********************* tri_partition_fusion *******************)
 
@@ -75,3 +79,4 @@ let tri_partition_fusion inf l =
 
 (********************** fonction de tri finale **********************)
 
+(* faire des tests et trouver la fonction de tri la plus efficace *)
