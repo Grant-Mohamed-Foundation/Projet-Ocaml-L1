497ff87035aa7ec31ee3b8cae7a9944ba3bc8daf
IhFisk <ihfisk42@gmail.com>
2017-04-29 19:11:18 +0200

    ajout de det et sca

--- proj.ml
+++ proj.ml
@@ -18,5 +18,19 @@ let infc point1 point2 =
     else if point1.y = point2.y && point1.x <= point2.x
          then true
          else false ;;
-
          
+         
+(* det *)
+
+let det q0 q1 q2 =
+    let q0q1 = { x = q1.x - q0.x ; y = q1.y - q0.y }
+    and q0q2 = { x = q2.x - q0.x ; y = q2.y - q0.y } in
+        (q0q1.x * q0q2.y) - (q0q2.x * q0q1.y) ;;
+        
+        
+(* sca *)
+
+let sca q0 q1 q2 =
+    let q0q1 = { x = q1.x - q0.x ; y = q1.y - q0.y }
+    and q0q2 = { x = q2.x - q0.x ; y = q2.y - q0.y } in
+        (q0q1.x * q0q2.x) + (q0q2.y * q0q1.y) ;;
