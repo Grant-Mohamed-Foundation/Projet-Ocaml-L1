37d04996c7d11947d82a789e586fbd488b13f275
IhFisk <ihfisk42@gmail.com>
2017-05-12 16:23:31 +0200

    Update proj.ml

--- proj.ml
+++ proj.ml
@@ -7,10 +7,12 @@
           (*******************************)
 
 
+(* chargement des modules *)
 #load "list_sup.cmo" ;;
 #load "point.cmo" ;;
 #load "pile.cmo" ;;
 
+(* ouverture des modules *)
 open List_sup ;;
 open Point ;;
 open Pile ;;
@@ -22,11 +24,15 @@ let infc point1 point2 =
     if point1.y < point2.y
     then true
     else point1.y = point2.y && point1.x <= point2.x ;;
-         
+
+(* val infc : point -> point -> bool *)
+    
          
 (* det *)
 let det q0 q1 q2 =
     (q1.x - q0.x)*(q2.y - q0.y) - (q2.x - q0.x) * (q1.y - q0.y);;
+        
+(* val det : point -> point -> point -> int *)
         
         
 (* sca *)
@@ -34,6 +40,8 @@ let det q0 q1 q2 =
 let sca q0 q1 q2 =
     (q1.x - q0.x)*(q2.x - q0.x) + (q1.y - q0.y)*(q2.y - q0.y);;
 
+(* val sca : point -> point -> point -> int *)
+    
         
 (* infg *)
 
@@ -43,16 +51,19 @@ let infg w p1 p2 =
     else if p1 != p2 && p2 != w && p1 != w
          then (det w p1 p2) > 0
          else (det w p1 p2) = 0 && (sca p1 w p2) < 0 ;;
-                   
+         
+(* val infg : point -> point -> point -> bool *)
+         
 
-(* test de tri_points, ca marche avec le test juste au dessus apres faut voir avec d'autre xD *)
+(* tri_points *)
 
 let tri_points listePoint =
      suppr_doublons (tri (infg (min_list infc listePoint)) listePoint);;
 
+(* val tri_points : point list -> point list *)
+     
      
 (* algo_graham *)
-(* Je pense que ça fonctionne presque mais je trouve pas l'erreur xD *)
 
 let rec algo_graham liste pile =
     let s = subtop pile
@@ -64,7 +75,9 @@ let rec algo_graham liste pile =
                  else if det p x s < 0
                       then algo_graham liste (depiler pile)
                       else algo_graham r (empiler x (depiler pile)) ;;
-                       
+
+(* val algo_graham : point list -> point pile -> point pile *)
+                      
                        
 (* env_graham *)
 
@@ -75,6 +88,9 @@ let env_graham listePoint =
         |[x] -> failwith "Pas assez d'element dans la liste"
         |x::y::r -> list_of_pile(algo_graham r (empiler y (empiler x vide))) ;;
 
+(* val env_graham : point list -> point list *)
+        
+        
 (* env *)
 
 let env g n =
@@ -85,3 +101,4 @@ let env g n =
         tracer_polygone (env_graham l)
     ) ;;
 
+(* val env : ('a -> nuage) -> 'a -> unit *)
